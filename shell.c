1832044584f527abb78450f357ebc4db08eb9eb7
waga43 <waga43tech@gmail,com>
2022-11-16 14:25:08 -0800

    Added a read_line function

---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* read_line: Function reads a line of code
+* DESCRIPTION: Reads a line of code
+*
+* Return: line
+*
+*/
+
+char *read_line(void)
+{
+	char *line;
+	size_t n = 0;
+	size_t num_line;
+
+	getline(&line, &n, stdin);
+	return (line);
+}
