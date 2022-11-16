f1c425df3e723cfd6ca64d932af20b39e3df5fa2
Sanni Ozigi <sandaev20@gmail.com>
2022-11-16 14:12:28 -0800

    main.c first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *prompt = "cisfun$ ";
+	char *line;
+	char **tokens = NULL;
+	size_t n = 0;
+	int i;
+	int status = 1;
+
+	while (status)
+	{
+		printf("%s", prompt);
+		line = read_line();
+		tokens = split_line(line);
+		execute(tokens);
+	}
+	free(line);
+	free(tokens);
+}
